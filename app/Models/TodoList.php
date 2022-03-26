bc162a5d4d8510ca947e483d5bd3f2e290703c10
Vladimer Keinashvili <vovakeinashvili@gmali.com>
2022-03-26 21:18:06 +0400

    Initial

--- app/Models/TodoList.php
+++ app/Models/TodoList.php
@@ -11,35 +11,3 @@ class TodoList extends Model
 
     protected $fillable = ['content'];
 }
-
-
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-
-class CreateTodoListsTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('todo_lists', function (Blueprint $table) {
-            $table->id();
-            $table->string('content');
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('todo_lists');
-    }
-}
